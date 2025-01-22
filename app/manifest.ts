f80ad1cfc079b46947f392dc28b578f4499ffffa
Jason Aw <jason_aw1998@hotmail.com>
2025-01-22 21:18:33 +0800

    change favicon

--- app/manifest.ts
+++ app/manifest.ts
@@ -3,15 +3,17 @@ import type { MetadataRoute } from "next";
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "IDRead Workforce",
-    short_name: "IDREAD WF",
-    description: "IDRead Workforce is a workforce management system that uses AI to automate the process of verifying the identity of employees.",
+    short_name: "IDREAD Workforce",
+    description:
+      "IDRead Workforce is a workforce management system that uses AI to automate the process of verifying the identity of employees.",
     start_url: "/",
+    display_override: ["browser", "minimal-ui", "fullscreen"],
     display: "standalone",
     background_color: "#ffffff",
     theme_color: "#ffffff",
     icons: [
       {
-        src: "/aidaicon.png",
+        src: "/icon.png",
         sizes: "512x512",
         type: "image/png",
       },
